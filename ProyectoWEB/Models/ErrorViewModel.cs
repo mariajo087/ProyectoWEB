31887c5760dc70578c5813f0946130bae3851795
dtamayob <valenblandon10@gmail.com>
2025-05-08 23:38:49 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
